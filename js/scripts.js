365ecbd85d45f8da66c9ed581438ed4a1d507e90
Drew Yoxsimer and Philip Eskins <drew@>
2019-01-17 16:26:08 -0800

    refactor logic in compareResults function

--- js/scripts.js
+++ js/scripts.js
@@ -20,28 +20,52 @@ function choose() {
 
 function compareResults(computerMove, playerMove){
 
-  if (computerMove === "Scissor" && playerMove === "Rock") {
-    $(".playResults").text("Player Wins!");
-    win++;}
-  if (computerMove === "Scissor" && playerMove === "Paper") {
-    $(".playResults").text("Computer Wins!"); }
-
-  if (computerMove === "Paper" && playerMove === "Rock") {
-    $(".playResults").text("Computer Wins!"); }
-
-  if (computerMove === "Paper" && playerMove === "Scissor") {
-    $(".playResults").text("Player Wins");
-    win++;}
-
-  if (computerMove === "Rock" && playerMove === "Scissor") {
-    $(".playResults").text("Computer Wins!"); }
-
-  if (computerMove === "Rock" && playerMove === "Paper") {
-    $(".playResults").text("Player Wins");
-    win++;}
-
-  if (computerMove===playerMove) {
+  if(playerMove === "Rock"){
+    if(computerMove === "Paper") {
+      $(".playResults").text("Computer Wins!"); }
+    if(computerMove === "Scissor") {
+      $(".playResults").text("Player Wins!");
+      win++;}
+  }
+  if(playerMove === "Paper"){
+    if(computerMove === "Scissor") {
+      $(".playResults").text("Computer Wins!"); }
+    if(computerMove === "Rock") {
+      $(".playResults").text("Player Wins!");
+      win++;}
+    }
+  if(playerMove === "Scissor"){
+    if(computerMove === "Rock") {
+      $(".playResults").text("Computer Wins!"); }
+    if(computerMove === "Paper") {
+      $(".playResults").text("Player Wins!");
+      win++;}
+    }
+  if(playerMove === computerMove) {
     $(".playResults").text("It's a tie!");}
+
+  // if (computerMove === "Scissor" && playerMove === "Rock") {
+  //   $(".playResults").text("Player Wins!");
+  //   win++;}
+  // if (computerMove === "Scissor" && playerMove === "Paper") {
+  //   $(".playResults").text("Computer Wins!"); }
+  //
+  // if (computerMove === "Paper" && playerMove === "Rock") {
+  //   $(".playResults").text("Computer Wins!"); }
+  //
+  // if (computerMove === "Paper" && playerMove === "Scissor") {
+  //   $(".playResults").text("Player Wins");
+  //   win++;}
+  //
+  // if (computerMove === "Rock" && playerMove === "Scissor") {
+  //   $(".playResults").text("Computer Wins!"); }
+  //
+  // if (computerMove === "Rock" && playerMove === "Paper") {
+  //   $(".playResults").text("Player Wins");
+  //   win++;}
+  //
+  // if (computerMove===playerMove) {
+  //   $(".playResults").text("It's a tie!");}
 
   // if (playerMove ==="Rock") {
   //   playValue = 1;
